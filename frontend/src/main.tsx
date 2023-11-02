3f287cb554465721b0f1c4f7a776d5f0f95e8afc
Iann Bika <Ianngeorges@gmail.com>
2023-11-02 16:06:54 -0500

    frontend finished (screens/components, routes, redux-toolkit)

---
+++ frontend/src/main.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from "react-router-dom";
+import store from "./store.ts";
+import { Provider } from "react-redux";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import PrivateRoute from "./components/PrivateRoute.tsx";
+import HomeScreen from "./screens/HomeScreen.tsx";
+import LoginScreen from "./screens/LoginScreen.tsx";
+import RegisterScreen from "./screens/RegisterScreen.tsx";
+import ProfileScreen from "./screens/ProfileScreen.tsx";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route index={true} path="/" element={<HomeScreen />} />
+			<Route path="/login" element={<LoginScreen />} />
+			<Route path="/register" element={<RegisterScreen />} />
+			{/* Private Route */}
+			<Route path="" element={<PrivateRoute />}>
+				<Route
+					path="/profile"
+					element={<ProfileScreen />}
+				/>
+			</Route>
+		</Route>
+	)
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<Provider store={store}>
+		<React.StrictMode>
+			<RouterProvider router={router} />
+		</React.StrictMode>
+	</Provider>
+);
